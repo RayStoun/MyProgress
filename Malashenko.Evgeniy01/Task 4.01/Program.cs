abb07162296a862e3a82c4635c10d422e4fb6ecb
RayStoun <stigmatory3@gmail.com>
2018-03-01 19:22:52 +0300

    Add Task1 Completed

---
+++ Malashenko.Evgeniy01/Task 4.01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size triangle");
+            int size = Int32.Parse(Console.ReadLine());
+            if (size<=0)
+            {
+                Console.WriteLine("Error! Size must be more zero! ");
+            }
+            else
+            {
+                DrawsIndentation(size);
+            }
+        }
+
+        static private void DrawsIndentation(int size)
+        {
+            string resulstring = "";
+            for (int i = 1; i <size ; i++)
+            {
+                resulstring = "";
+                int numberspace = size -i;
+                resulstring = resulstring.PadLeft(numberspace,' ');
+                resulstring = resulstring + new string('*', (i*2)-1);
+                Console.WriteLine(resulstring);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Malashenko.Evgeniy01/Task 4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Triangle creation program");
+            Console.WriteLine("Enter number ");
+            int size =Int32.Parse(Console.ReadLine());
+            int numberindentation = size-1;
+            string drawsStars = "";
+            string numberStars = "";
+            while (size <= 0)
+            {
+                Console.WriteLine("Size must be more zero!\r Pleae try again");
+                 size = Int32.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i <size; i++)
+            {  
+                numberStars = new string('*',(i*2)>0?(i*2)-1:(i));
+                drawsStars = DrawsIndentation(numberindentation)+numberStars;
+                numberindentation--;
+                
+                Console.WriteLine(drawsStars);
+            }
+            Console.ReadKey();
+        }
+        static private string DrawsIndentation(int sizeIndentation)
+        {
+            string indentation = "";
+            indentation = indentation.PadLeft(sizeIndentation);
+            return indentation;
+           
+        }
+
+    }
+}
