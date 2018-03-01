fcd15a17458e126a6454d0c309377a2b67a94381
RayStoun <stigmatory3@gmail.com>
2018-03-01 17:04:18 +0300

    Add Task1 Completed

---
+++ Malashenko.Evgeniy01/task 5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size triangle");
+            int size = Int32.Parse(Console.ReadLine());
+            if (size <= 0)
+            {
+                Console.WriteLine("Error! Size must be more zero! ");
+            }
+            else
+            {
+                DrawsIndentation(size);
+            }
+        }
+        static private void DrawsIndentation(int size)
+        {
+            string resulstring = "";
+            for (int i = 1; i < size; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    resulstring = "";
+                    int numberspace = size - j;
+                    resulstring = resulstring.PadLeft(numberspace, ' ');
+                    resulstring = resulstring + new string('*', (j * 2) - 1);
+                    Console.WriteLine(resulstring);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
